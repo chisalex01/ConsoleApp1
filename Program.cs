36b3e8cffda6fe49b0ccf78324b41c30bea5103e
Chis Alexandru <chisalex01@yahoo.com>
2021-03-25 10:37:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IBank ing = new ING();
+            IBank bt = new BT();
+            Console.WriteLine($"ING converts 100 EUR into {ING.");
+            Console.ReadKey();
+        }
+    }
+
+    /*
+        fiecare banca are propriul schimb valutar
+        ING: 1 EUR = 4.8554 RON
+        BT: 1 EUR = 4.7554 RON
+        GOAL: Create banks that exchange from EUR to RON currency
+    */
+
+    public interface IBank // BT, ING
+    {
+        decimal ExchangeInRON(decimal valueInRON);
+    }
+
+    public interface IINGExchange // BT, ING
+    {
+        decimal Value();
+    }
+
+    public class ING : IBank
+    {
+        public decimal ExchangeInRON(decimal valueInRON)
+        {
+            decimal EURtoRONExchangeRate = 4.8554M;
+
+            return valueInEUr * exchangeRate.Value();
+        }
+
+        public decimal ExchangeInRON(decimal valueInRON)
+        {
+            decimal EURtoRONExchangeRate = 4.8554M;
+
+            return valueInEUr * exchangeRate.Value();
+        }
+
+    }
+
+}
